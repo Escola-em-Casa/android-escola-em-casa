92b538f94f4e3fc81b4785da49d26bde24ba8db6
João Guilherme <joaaogui@gmail.com>
2020-06-18 22:03:42 -0300

    Adds splash screen and new logos

---
+++ app/src/main/java/org/cordova/quasar/corona/app/AboutFragment.java
@@ -0,0 +1,21 @@
+package org.cordova.quasar.corona.app;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AboutFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    public static AboutFragment newInstance() {
+        return new AboutFragment();
+    }
+}
