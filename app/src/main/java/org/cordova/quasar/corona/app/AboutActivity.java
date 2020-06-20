f8864027b3d70138054d8bbc4444ec4a23303984
João Guilherme <joaaogui@gmail.com>
2020-06-20 15:55:01 -0300

    Changes code from fragments to activities

---
+++ app/src/main/java/org/cordova/quasar/corona/app/AboutActivity.java
@@ -0,0 +1,42 @@
+package org.cordova.quasar.corona.app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        BottomNavigationView navigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        navigationView.setSelectedItemId(R.id.about);
+        navigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.classroom: {
+                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                overridePendingTransition(0, 0);
+                                return true;
+                            }
+                            case R.id.wikipedia: {
+                                startActivity(new Intent(getApplicationContext(), WikipediaActivity.class));
+                                overridePendingTransition(0, 0);
+                                return true;
+                            }
+                            case R.id.about:
+                                return true;
+                        }
+                        return false;
+                    }
+                }
+        );
+    }
+}
