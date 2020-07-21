d88754ba8618a3fce2e5395f63dca0836240421a
guilacerda <guiglacerda@gmail.com>
2020-07-21 12:24:48 -0300

    Creates the FAQ activity

---
+++ app/src/main/java/org/cordova/quasar/corona/app/QuestionsActivity.java
@@ -0,0 +1,15 @@
+package org.cordova.quasar.corona.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class QuestionsActivity  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+}
