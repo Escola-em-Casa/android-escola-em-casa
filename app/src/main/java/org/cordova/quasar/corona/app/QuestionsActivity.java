5c801806fd91dba8c024b44d80fdad688359d67d
guilacerda <guiglacerda@gmail.com>
2020-07-21 12:49:24 -0300

    Adds the navigationbar to faqpage

--- app/src/main/java/org/cordova/quasar/corona/app/QuestionsActivity.java
+++ app/src/main/java/org/cordova/quasar/corona/app/QuestionsActivity.java
@@ -10,6 +10,43 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class QuestionsActivity  extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_questions);
+
+        BottomNavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setSelectedItemId(R.id.questions);
+
+        navigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    switch (item.getItemId()) {
+                        case R.id.classroom:
+                            startActivity(new Intent(getApplicationContext(), WebviewActivity.class)
+                                    .putExtra("url", "https://classroom.google.com/a/estudante.se.df.gov.br"));
+                            overridePendingTransition(0, 0);
+                            navigationView.getMenu().getItem(0).setChecked(true);
+                            return true;
+                        case R.id.wikipedia:
+                            startActivity(new Intent(getApplicationContext(), WebviewActivity.class)
+                                    .putExtra("url", "https://pt.wikipedia.org/"));
+                            overridePendingTransition(0, 0);
+                            navigationView.getMenu().getItem(1).setChecked(true);
+                            return true;
+                        case R.id.about:
+                            startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+                            overridePendingTransition(0, 0);
+                            navigationView.getMenu().getItem(2).setChecked(true);
+                            return true;
+                        case R.id.questions:
+                            startActivity(new Intent(getApplicationContext(), QuestionsActivity.class));
+                            overridePendingTransition(0, 0);
+
+                            navigationView.getMenu().getItem(3).setChecked(true);
+
+                            return true;
+                    }
+                    return false;
+                }
+        );
     }
 }
