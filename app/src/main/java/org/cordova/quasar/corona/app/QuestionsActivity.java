e112d51b301cbbcc484e0c19311e2eb77275b19b
Marco Antonio <markinlimac@gmail.com>
2020-10-14 15:48:35 -0300

    Added guided tour struct at questions activity (#30)

--- app/src/main/java/org/cordova/quasar/corona/app/QuestionsActivity.java
+++ app/src/main/java/org/cordova/quasar/corona/app/QuestionsActivity.java
@@ -7,7 +7,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import smartdevelop.ir.eram.showcaseviewlib.GuideView;
+import smartdevelop.ir.eram.showcaseviewlib.config.DismissType;
+import smartdevelop.ir.eram.showcaseviewlib.config.Gravity;
+import smartdevelop.ir.eram.showcaseviewlib.listener.GuideListener;
+
+import android.content.SharedPreferences;
+
 public class QuestionsActivity extends AppCompatActivity {
+    private void checkFirstRun() {
+      final String PREFS_NAME = "questions_first_run";
+      final String PREF_VERSION_CODE_KEY = "1.0";
+      final int DOESNT_EXIST = -1;
+
+      // Get current version code
+      int currentVersionCode = BuildConfig.VERSION_CODE;
+
+      // Get saved version code
+      SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+      int savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST);
+
+      // Check for first run or upgrade
+      if (currentVersionCode == savedVersionCode) {
+
+          // This is just a normal run
+          return;
+
+      } else if (savedVersionCode == DOESNT_EXIST) {
+        new GuideView.Builder(this)
+            .setTitle("Dúvidas Frequentes")
+            .setContentText("Esta aba serve para acessar as dúvidas frequentes")
+            .setGravity(Gravity.auto) //optional
+            .setDismissType(DismissType.anywhere) //optional - default DismissType.targetView
+            .setTargetView(findViewById(R.id.questions))
+            .setContentTextSize(12)//optional
+            .setTitleTextSize(14)//optional
+            .build()
+            .show();
+      } else if (currentVersionCode > savedVersionCode) {
+
+          // TODO This is an upgrade
+      }
+
+      // Update the shared preferences with the current version code
+      prefs.edit().putInt(PREF_VERSION_CODE_KEY, currentVersionCode).apply();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +93,8 @@ public class QuestionsActivity extends AppCompatActivity {
                     return false;
                 }
         );
+
+        checkFirstRun();
     }
 
     @Override
