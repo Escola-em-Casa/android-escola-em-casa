3bb28149cd33177da6a413b140daf6754f76b245
LGomees <lucasgomesjk@gmail.com>
2021-03-02 19:35:22 -0300

    :sparkles: add: Adding Questions Class

---
+++ app/src/main/java/org/cordova/quasar/corona/app/Questions.java
@@ -0,0 +1,32 @@
+package org.cordova.quasar.corona.app;
+
+public class Questions {
+
+    String question;
+    String answer;
+    boolean isExpanded;
+
+    public boolean isExpanded() {
+        return isExpanded;
+    }
+
+    public void setExpanded(boolean expanded) {
+        isExpanded = expanded;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
