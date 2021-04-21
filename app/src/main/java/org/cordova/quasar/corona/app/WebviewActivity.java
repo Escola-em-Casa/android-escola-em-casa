6141579a1b138fafb6bb429fd38b5e1d8a1dcb54
Pedro Igor <pedroigor.silva@outlook.com>
2021-04-21 12:44:13 -0300

    Use variables to tell a history on WebViewActivity/onKeyDown

--- app/src/main/java/org/cordova/quasar/corona/app/WebviewActivity.java
+++ app/src/main/java/org/cordova/quasar/corona/app/WebviewActivity.java
@@ -331,12 +331,12 @@ public class WebviewActivity extends AppCompatActivity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+        boolean isKeyPressedEqualsBack = keyCode == KeyEvent.KEYCODE_BACK;
+        boolean isKeyBackPressedAndCanThisGoBack = isKeyPressedEqualsBack && myWebView.canGoBack();
+        if (isKeyBackPressedAndCanThisGoBack) {
             myWebView.goBack();
-
             return true;
         }
-
         return super.onKeyDown(keyCode, event);
     }
 
